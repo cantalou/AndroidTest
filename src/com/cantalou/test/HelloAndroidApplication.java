a252be906a4ac332bfbf58811501d5515bb2dc66
cantalou <cantalou89@gmail.com>
2015-12-08 11:15:17 +0800

    1

---
+++ src/com/cantalou/test/HelloAndroidApplication.java
@@ -0,0 +1,12 @@
+package com.cantalou.test;
+
+import android.app.Application;
+import com.cantalou.skin.SkinManager;
+
+public class HelloAndroidApplication extends Application {
+
+    @Override
+    public void onCreate() {
+	super.onCreate();
+    }
+}
