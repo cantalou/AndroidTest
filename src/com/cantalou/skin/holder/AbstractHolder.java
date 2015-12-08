31795fda6f7646c8e0fea277d621d75dc1d047c3
cantalou <cantalou89@gmail.com>
2015-12-08 23:34:50 +0800

    1

--- src/com/cantalou/skin/holder/AbstractHolder.java
+++ src/com/cantalou/skin/holder/AbstractHolder.java
@@ -5,71 +5,55 @@ import android.view.View;
 
 import com.cantalou.skin.holder.AttrHolder;
 
-public abstract class AbstractHolder implements AttrHolder
-{
+public abstract class AbstractHolder implements AttrHolder, Cloneable {
 
-    /**
-     * 父类的parse是否有被调用
-     */
-    private boolean called = false;
+	/**
+	 * 父类的parse是否有被调用
+	 */
+	private boolean called = false;
 
-    @Override
-    public final void parse(View view, AttributeSet attrs)
-    {
-        called = false;
-        boolean result = parse(attrs);
-        if (!called)
-        {
-            throw new IllegalStateException("super parse(AttributeSet attrs) must be call");
-        }
-        if (result)
-        {
-            view.setTag(ATTR_HOLDER_KEY, this);
-        }
-    }
+	@Override
+	public final void parse(View view, AttributeSet attrs) {
+		called = false;
+		boolean result = parse(attrs);
+		if (!called) {
+			throw new IllegalStateException("super parse(AttributeSet attrs) must be call");
+		}
+		if (result) {
+			view.setTag(ATTR_HOLDER_KEY, this);
+		}
+	}
 
-    /**
-     * 解析组件属性
-     *
-     * @param attrs
-     * @return 组件使用app资源 true
-     */
-    protected boolean parse(AttributeSet attrs)
-    {
-        called = true;
-        return false;
-    }
+	/**
+	 * 解析组件属性
+	 *
+	 * @param attrs
+	 * @return 组件使用app资源 true
+	 */
+	protected boolean parse(AttributeSet attrs) {
+		called = true;
+		return false;
+	}
 
-    protected int getResourceId(AttributeSet attrs, String name)
-    {
-        int len = attrs.getAttributeCount();
-        for (int i = 0; i < len; i++)
-        {
-            String attributeName = attrs.getAttributeName(i);
-            if (name.equals(attributeName))
-            {
-                String value = attrs.getAttributeValue(i);
-                int ik = attrs.getAttributeResourceValue(i, 0);
-                int id = attrs.getAttributeNameResource(i);
-                int j = attrs.getAttributeIntValue(i, 0);
-                if (value.startsWith("@"))
-                {
-                    id = Integer.parseInt(value.substring(1));
-                    return (id & APP_RESOURCE_ID_PACKAGE) == APP_RESOURCE_ID_PACKAGE ? id : 0;
-                }
+	protected int getResourceId(AttributeSet attrs, String name) {
+		int len = attrs.getAttributeCount();
+		for (int i = 0; i < len; i++) {
+			String attributeName = attrs.getAttributeName(i);
+			if (name.equals(attributeName)) {
+				int id = attrs.getAttributeResourceValue(i, 0);
+				return (id & APP_RESOURCE_ID_PACKAGE) == APP_RESOURCE_ID_PACKAGE ? id : 0;
+			}
+		}
+		return 0;
+	}
 
-            }
-        }
-        return 0;
-    }
-
-    protected int getResourceId(AttributeSet attrs, int index)
-    {
-        String attributeName = attrs.getAttributeName(index);
-        String value = attrs.getAttributeValue(index);
-        int i = attrs.getAttributeResourceValue(index, 0);
-        int id = attrs.getAttributeNameResource(index);
-        int j = attrs.getAttributeIntValue(index, 0);
-        return (id & APP_RESOURCE_ID_PACKAGE) == APP_RESOURCE_ID_PACKAGE ? id : 0;
-    }
+	protected int getResourceId(AttributeSet attrs, int index) {
+		int id = attrs.getAttributeResourceValue(index, 0);
+		return (id & APP_RESOURCE_ID_PACKAGE) == APP_RESOURCE_ID_PACKAGE ? id : 0;
+	}
+	
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
 }
