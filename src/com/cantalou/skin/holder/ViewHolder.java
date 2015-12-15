90331d56ad598be1fa15b938cdbf99ff02e95c9e
cantalou <cantalou89@gmail.com>
2015-12-15 10:31:46 +0800

    1

--- src/com/cantalou/skin/holder/ViewHolder.java
+++ src/com/cantalou/skin/holder/ViewHolder.java
@@ -7,25 +7,31 @@ import android.view.View;
 import com.cantalou.skin.SkinManager;
 import com.cantalou.skin.holder.AbstractHolder;
 
-public class ViewHolder extends AbstractHolder {
+public class ViewHolder extends AbstractHolder
+{
 
-	protected int background;
+    protected int background;
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void reload(View view, Resources res) {
-		if (background != 0) {
-			view.setBackgroundDrawable(res.getDrawable(background));
-		}
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    public void reload(View view, Resources res)
+    {
+        if (background != 0)
+        {
+            view.setBackgroundDrawable(res.getDrawable(background));
+        }
+    }
 
-	@Override
-	public boolean parseAttr(AttributeSet attrs) {
-		background = getResourceId(attrs, "background");
-		if (background != 0) {
-			SkinManager.getInstance().registerDrawable(background);
-		}
-		return super.parseAttr(attrs) || background != 0;
-	}
+    @Override
+    public boolean parseAttr(AttributeSet attrs)
+    {
+        background = getResourceId(attrs, "background");
+        if (background != 0)
+        {
+            SkinManager.getInstance()
+                       .registerDrawable(background);
+        }
+        return super.parseAttr(attrs) || background != 0;
+    }
 
 }
