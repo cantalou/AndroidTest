8de99a9a7ac94d29d7ac0cf7d0bf74d2e5a0b39a
cantalou <cantalou89@gmail.com>
2016-01-13 22:26:42 +0800

    1

--- src/com/cantalou/skin/array/DrawableLongSpareArray.java
+++ src/com/cantalou/skin/array/DrawableLongSpareArray.java
@@ -13,44 +13,36 @@ import com.cantalou.skin.res.ProxyResources;
 import com.cantalou.skin.res.SkinProxyResources;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-public class DrawableLongSpareArray extends LongSparseArray<Drawable.ConstantState>
-{
+public class DrawableLongSpareArray extends LongSparseArray<Drawable.ConstantState> {
 
-    private LongSparseArray<Integer> resourceIdKeyMap;
+	private LongSparseArray<Integer> resourceIdKeyMap;
 
-    /**
-     * Resources mColorStateListCache
-     */
-    private LongSparseArray<Drawable.ConstantState> originalCache;
+	/**
+	 * Resources mColorStateListCache
+	 */
+	private LongSparseArray<Drawable.ConstantState> originalCache;
 
-    private ProxyResources resources;
+	private ProxyResources resources;
 
-    public DrawableLongSpareArray(ProxyResources resources, LongSparseArray<Drawable.ConstantState> originalCache, LongSparseArray<Integer> resourceIdKeyMap)
-    {
-        this.resources = resources;
-        this.originalCache = originalCache;
-        this.resourceIdKeyMap = resourceIdKeyMap;
-    }
+	public DrawableLongSpareArray(ProxyResources resources, LongSparseArray<Drawable.ConstantState> originalCache,
+			LongSparseArray<Integer> resourceIdKeyMap) {
+		this.resources = resources;
+		this.originalCache = originalCache;
+		this.resourceIdKeyMap = resourceIdKeyMap;
+	}
 
-    @Override
-    public Drawable.ConstantState get(long key)
-    {
-        Integer id;
-        if (resources != null && (id = resourceIdKeyMap.get(key)) != null)
-        {
-            Drawable dr = resources.loadDrawable(id);
-            if (dr != null)
-            {
-                return dr.getConstantState();
-            }
-            else
-            {
-                return null;
-            }
-        }
-        else
-        {
-            return originalCache.get(key);
-        }
-    }
+	@Override
+	public Drawable.ConstantState get(long key) {
+		Integer id;
+		if (resources != null && (id = resourceIdKeyMap.get(key)) != null) {
+			Drawable dr = resources.loadDrawable(id);
+			if (dr != null) {
+				return dr.getConstantState();
+			} else {
+				return null;
+			}
+		} else {
+			return originalCache.get(key);
+		}
+	}
 }
